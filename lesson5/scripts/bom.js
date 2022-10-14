d6b9e9d07c9a53a7911280d1d5f40264ff0c855d
seanryanbyu <seanryan@byui.edu>
2022-10-13 22:10:23 -0700

    update

---
+++ lesson5/scripts/bom.js
@@ -0,0 +1,21 @@
+const entry = document.querySelector('input');
+const btn =document.querySelector('button');
+const list = document.querySelector('ul');
+
+btn.addEventListener('click', function() {
+    const chapter =  entry.value;
+    entry.value= "";
+    const newList = document.createElement('li');
+    const newBtn = document.createElement('button');
+    const newSpan = document.createElement('span');
+    newSpan.textContent = chapter;
+    newBtn.textContent = '❌';
+    newList.appendChild(newSpan)
+    newList.appendChild(newBtn);
+    list.appendChild(newList);
+    newBtn.addEventListener('click', function(){
+        list.removeChild(newList);
+    })
+
+    
+})
