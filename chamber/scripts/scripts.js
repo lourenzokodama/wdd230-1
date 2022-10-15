a9c0d7eca3fdb81a7051d606fad85e518ece1282
seanryanbyu <seanryan@byui.edu>
2022-10-15 14:39:16 -0700

    latest update

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -9,6 +9,13 @@ const ham = document.getElementById("hamburgerBtn");
 
 ham.onclick = toggleMenu;
 
-
+// footer date
 const date = new Date(document.lastModified);
 document.getElementById("updateDate").innerHTML = document.lastModified;
+
+
+// header date
+const today = new Date();
+const longDay = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(today);
+const headerDate = document.querySelector(".headerDate");
+headerDate.innerHTML = `${longDay}`;
